9601c088f9550c96611ad0ccb0eeba30812ff332
Niko Hoffren <niko.hoffren@gmail.com>
2023-06-21 18:44:34 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import tsconfigPaths from "vite-plugin-tsconfig-paths";
-import WindiCSS from 'vite-plugin-windicss'
+import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh(), tsconfigPaths(), WindiCSS()],
     resolve: {
-        alias: [
-            // Add any custom aliases you need for your project
-        ],
+        alias: [],
     },
     build: {
-        // This is optional, but you can specify the target for the transpiled code
         target: "esnext",
     },
 });
