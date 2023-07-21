2a6c609cc1edb3c2d69a02c6ddea131c1bf9896f
Niko Hoffrén <niko.hoffren@gmail.com>
2023-07-21 16:39:01 +0300

    Add Spinner when loading images in ImageCarousel

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,11 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import { LanguageContext } from "./LanguageContext";
+import Spinner from "../components/Spinner";
 
-export default function ImageCarousel() {
-    const { language } = useContext(LanguageContext) as {
-        language: string;
-        setLanguage: (language: string) => void;
+export default function Gallery() {
+    // const { language } = useContext(LanguageContext) as {
+    //     language: string;
+    //     setLanguage: (language: string) => void;
+    // };
+    const [isLoading, setIsLoading] = useState(true);
+    const imageLoaded = () => {
+        setIsLoading(false);
     };
 
     return (
@@ -18,40 +23,51 @@ export default function ImageCarousel() {
                     className="image-carousel-img"
                 >
                     <div>
+                        {isLoading && <Spinner />}
                         <img src="gallery1.png" alt="" />
                         {/* <p className="legend">
                             {language === "fi" ? "Mehiläiset" : "Bees"}
                         </p> */}
                     </div>
                     <div>
-                        <img src="gallery2.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery2.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="gallery3.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery3.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="gallery4.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery4.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="gallery5.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery5.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="gallery6.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery6.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="gallery7.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery7.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="gallery8.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery8.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="gallery9.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery9.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="gallery10.png" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="gallery10.png" alt="" onLoad={imageLoaded} />
                     </div>
                     <div>
-                        <img src="HHkuva5.jpg" alt="" />
+                        {isLoading && <Spinner />}
+                        <img src="HHkuva5.jpg" alt="" onLoad={imageLoaded} />
                     </div>
                 </Carousel>
             </div>
